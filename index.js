03a2f7fddc705b37b8e8994911a6b21b5d97a343
kuawo <63333494+kuawo@users.noreply.github.com>
2022-03-30 23:33:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ router.get("/api/wx_openid", async (ctx) => {
 
 // 发送模板消息
 router.post("/api/kw_template_send", async (ctx) => {
-  new Promise((resolve, reject) => {
+  let np = new Promise((resolve, reject) => {
     send_request({
       method: 'POST',
       url: 'https://api.weixin.qq.com/cgi-bin/message/template/send',
@@ -37,7 +37,9 @@ router.post("/api/kw_template_send", async (ctx) => {
       console.log('接口返回内容', response.body)
       resolve(JSON.parse(response.body))
     })
-  }).then((body) => {
+  })
+  
+  np.then((body) => {
       ctx.body = body
   })
 });
